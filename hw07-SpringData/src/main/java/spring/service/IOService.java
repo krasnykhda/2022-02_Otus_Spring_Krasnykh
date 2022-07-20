5c0dfa01990dc5e26ec65434c1ae951ac49484eb
dmitry <krasnykhda@gmail.com>
2022-07-20 10:03:29 +0300

    Commit2

--- hw07-SpringData/src/main/java/spring/service/IOService.java
+++
@@ -1,7 +0,0 @@
-package spring.service;
-
-public interface IOService {
-    void out(String message);
-
-    String readLn(String prompt);
-}
--- hw07-SpringData/src/test/resources/data.sql
+++
@@ -1,4 +0,0 @@
-insert into genres (id,name) values (4,'Роман');
-insert into genres (id,name) values (5,'Комедия');
-insert into authors (id,name) values (4,'Толстой');
-insert into authors (id,name) values (5,'Чехов');
