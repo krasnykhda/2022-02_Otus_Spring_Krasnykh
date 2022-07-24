87abdba1112f1aebb73c8937ff5ebb7316aa8995
dmitry <krasnykhda@gmail.com>
2022-07-24 18:48:18 +0300

    Commit2

--- hw06-ORM/src/main/java/spring/service/BookToStringConverter.java
+++ hw06-ORM/src/main/java/spring/service/BookToStringConverter.java
@@ -9,24 +9,22 @@ import java.util.List;
 @Service
 public class BookToStringConverter {
     public String getBookAsString(Book book) {
-        StringBuilder builder = new StringBuilder("\n");
-        builder.append("id книги: ");
-        builder.append(book.getId());
-        builder.append("\n");
-        builder.append("Название книги: ");
-        builder.append(book.getName());
-        builder.append("\n");
-        builder.append("Жанр книги: ");
-        builder.append(book.getGenre().getName());
-        builder.append("\n");
-        builder.append("Авторы книги: [");
-        builder.append("\n");
+        StringBuilder builder = new StringBuilder("\n").append("id книги: ")
+        .append(book.getId())
+        .append("\n")
+        .append(book.getName())
+        .append("\n")
+        .append("Жанр книги: ")
+        .append(book.getGenre().getName())
+        .append("\n")
+        .append("Авторы книги: [")
+        .append("\n");
         for (Author author : book.getAuthors()) {
-            builder.append("   Имя автора: ");
-            builder.append(author.getName());
+            builder.append("   Имя автора: ")
+            .append(author.getName());
         }
-        builder.append("\n");
-        builder.append("]");
+        builder.append("\n")
+        .append("]");
 
         return builder.toString();
     }
@@ -34,11 +32,9 @@ public class BookToStringConverter {
 
     public String getBookAsString(List<Book> books) {
         StringBuilder builder = new StringBuilder("\n");
-        var booksString = "";
         for (Book book : books) {
             builder.append(getBookAsString(book));
             builder.append("\n");
-           // booksString = booksString + getBookAsString(book) + "\n";
         }
         return builder.toString();
     }
